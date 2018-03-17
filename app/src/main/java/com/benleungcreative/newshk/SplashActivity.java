941d34a93db893fe942a4058b957196eb4e49e21
Ben Leung <benhk2005@gmail.com>
2018-03-17 12:38:19 +0800

    make splash activity UI

--- app/src/main/java/com/benleungcreative/newshk/SplashActivity.java
+++ app/src/main/java/com/benleungcreative/newshk/SplashActivity.java
@@ -1,12 +1,29 @@
 package com.benleungcreative.newshk;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final int LAUNCH_MAIN_ACTIVITY_DELAY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        launchMainActivityDelayed();
+    }
+
+    private void launchMainActivityDelayed() {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, LAUNCH_MAIN_ACTIVITY_DELAY);
     }
 }
