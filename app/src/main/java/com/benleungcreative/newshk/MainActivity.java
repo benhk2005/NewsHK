cdaf8eee5d17813022e913a952d89121ac41dacb
Ben Leung <benhk2005@gmail.com>
2018-03-17 12:53:26 +0800

    update main activity UI

--- app/src/main/java/com/benleungcreative/newshk/MainActivity.java
+++ app/src/main/java/com/benleungcreative/newshk/MainActivity.java
@@ -1,13 +1,21 @@
 package com.benleungcreative.newshk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mainToolbar;
+    private TabLayout mainTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainToolbar = findViewById(R.id.mainToolbar);
+        mainTabLayout = findViewById(R.id.mainTabLayout);
+
     }
 }
